630b83c055ee3a33bdd5bf8d6ff6813326e61661
svenehlers <sven.e.ehlers@gmail.com>
2017-09-20 20:02:58 +0200

    alpha v0.1
    changed .gitignore
    declared structs

--- main.c
+++ main.c
@@ -1,9 +1,49 @@
 
+#include <stdint.h>
 
-/**
- * main.c
- */
-int main(void)
+typedef struct Menu{
+
+    uint8_t menuPosition;
+
+    uint8_t curentSection;
+    uint8_t currentMode;
+    uint8_t currentParameter;
+
+    uint8_t currentParameterValue;
+
+    uint8_t chosedSections[10];
+
+    uint8_t parameter1;
+    uint8_t parameter2;
+    uint8_t parameter3;
+    uint8_t parameter4;
+
+    uint8_t activeLEDDisplayValue;
+    uint8_t activeRGBLEDValue;
+};
+
+typedef struct LEDSection{
+
+    uint8_t sectionActive;
+
+    uint8_t mode;
+
+    uint8_t parameter1;
+    uint8_t parameter2;
+    uint8_t parameter3;
+    uint8_t parameter4;
+};
+
+void main(void)
 {
-	return 0;
+
+    Menu menu;
+    LEDSection ledSection[10];
+
+    while(1)
+    {
+
+
+    }
+
 }
